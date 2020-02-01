a8ff085117bb406228dc4a489aedfe7c261f00b1
IbrahimMahdi97 <ibrahimmahdi115@gmail.com>
2020-02-02 01:51:06 +0300

    solved

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,78 +1,77 @@
-import React, { Component } from 'react';
-import ProteinForm from './ProteinForm';
-import FillingForm from './FillingForm';
-import ToppingForm from './ToppingForm';
-import SideForm from './SideForm';
+import React, { Component } from "react";
+import ProteinForm from "./ProteinForm";
+import FillingForm from "./FillingForm";
+import ToppingForm from "./ToppingForm";
+import SideForm from "./SideForm";
 
 const DEFAULT_STATE = {
   protein: [],
   fillings: [],
   toppings: [],
   sides: []
-}
+};
 
 class Form extends Component {
   state = {
     ...DEFAULT_STATE
-  }
+  };
 
-  handleSubmit(event) {
-    event.preventDefault()
-    document.getElementById("order-form").reset()
-    this.props.addOrder(this.state)
+  handleSubmit(props) {
+    this.event.preventDefault();
+    document.getElementById("order-form").reset();
+    props.addOrder(this.state);
 
     this.setState({
       ...DEFAULT_STATE
-    })
+    });
   }
 
   handleChange() {
-    const itemType = this.event.target.name
-    const item = this.event.target.value
+    const itemType = this.event.target.name;
+    const item = this.event.target.value;
 
-    !this.state[`${itemType}`].includes(item) ?
-      this.setState({
-        [itemType]: this.state[`${itemType}`].concat(item)
-      })
-    :
-      this.setState({
-        [itemType]: this.state[`${itemType}`].filter(
-          ingr => ingr !== item
-        )
-      })
+    !this.state[`${itemType}`].includes(item)
+      ? this.setState({
+          [itemType]: this.state[`${itemType}`].concat(item)
+        })
+      : this.setState({
+          [itemType]: this.state[`${itemType}`].filter(ingr => ingr !== item)
+        });
   }
 
   render() {
-    return(
+    return (
       <div className="ui raised container segment">
         <h1 className="ui block header">Order Form</h1>
-        <form className="ui form" id="order-form" onSubmit={ this.handleSubmit }>
+        <form className="ui form" id="order-form" onSubmit={this.handleSubmit}>
           <ProteinForm
-            protein={ this.state.protein }
-            handleOnChange={ this.handleChange }
+            protein={this.state.protein}
+            handleOnChange={this.handleChange}
           />
 
           <FillingForm
-            fillings={ this.state.fillings }
-            handleOnChange={ this.handleChange }
+            fillings={this.state.fillings}
+            handleOnChange={this.handleChange}
           />
 
           <ToppingForm
-            toppings={ this.state.toppings }
-            handleOnChange={ this.handleChange }
+            toppings={this.state.toppings}
+            handleOnChange={this.handleChange}
           />
 
           <SideForm
-            sides={ this.state.sides }
-            handleOnChange={ this.handleChange }
+            sides={this.state.sides}
+            handleOnChange={this.handleChange}
           />
 
           <br />
 
-          <button className="ui blue big button" type="submit">Submit</button>
+          <button className="ui blue big button" type="submit">
+            Submit
+          </button>
         </form>
       </div>
-    )
+    );
   }
 }
 
